c0b734766e27562840de32f6ff7d7f3304afd11d
luiz <luuizsgomes75@gmail.com>
2026-01-12 21:32:33 -0300

    desafio beecrowd 1095 e 1096

---
+++ src/beeSequenciaIJ_2.java
@@ -0,0 +1,11 @@
+public class beeSequenciaIJ_2 {
+    static void main(String[] args) {
+     //beecrowd 1096
+        for(int I =1; I <=9 ; I+=2){
+            for(int J = 7; J >=5 ; J--){
+                System.out.println("I=" + I + " J=" + J);
+            }
+        }
+    }
+
+}
---
+++ src/beeSequenciaIJ_I.java
@@ -0,0 +1,14 @@
+public class beeSequenciaIJ_I {
+    static void main(String[] args) {
+        //beecrowd 1095
+        int J = 60;
+        int I =1;
+
+     while(J>=0){
+         System.out.println("I=" + I + " J=" + J);
+         J-=5;
+         I+=3;
+     }
+
+    }
+}
