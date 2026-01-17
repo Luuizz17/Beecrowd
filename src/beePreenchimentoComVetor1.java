ce5a5f6bf4a9114875b3bb32345bf84c01c29f2d
Luiz Fernando Leal Gomes <luuizsgomes75@gmail.com>
2026-01-17 19:34:43 -0300

    Commit beecrowd desafio 1173 Preenchimento de Vetor I

---
+++ src/beePreenchimentoComVetor1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class beePreenchimentoComVetor1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] N = new int[10];
+         N[0]= sc.nextInt();
+
+        for(int i = 1; i < N.length ; i++) {
+            N[i] = N[i - 1] * 2;
+        }
+        for(int i =0 ; i < N.length ; i++) {
+            System.out.println("N[" + i + "] = " + N[i]);
+
+          }
+        }
+    }
+
