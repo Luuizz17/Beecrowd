a3b75c6a7ae98732aba1fe97aaef4fb10929e332
luiz <luuizsgomes75@gmail.com>
2026-02-24 11:28:38 -0300

    Exercicio Troca em Vetores I

---
+++ src/TrocaVetorI.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class TrocaVetorI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] N = new int[20];
+        int Y = 0;
+
+        for(int i = 0; i < N.length ; i++){
+            N[i] = sc.nextInt();
+        }
+        for(int i = 0; i< N.length/2 ; i++){
+            int temp = N[i];
+            N[i] = N[N.length - 1- i];
+            N[N.length - 1 - i ] = temp;
+        }
+        for(int i = 0 ; i < N.length ; i++){
+            System.out.println("N[" + i + "] = " + N[i]);
+        }
+
+    }
+}
